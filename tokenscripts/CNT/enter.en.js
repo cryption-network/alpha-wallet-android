dc81649c1df7a8909dd75316be6542e325e430e0
Gulshan Vasnani <gulshanslvasnani@gmail.com>
2021-07-07 17:52:11 +0530

    CNT xml to provide approval and enter to CNTStaker option

--- tokenscripts/CNT/enter.en.js
+++ tokenscripts/CNT/enter.en.js
@@ -6,7 +6,7 @@ class Token {
   }
 
   render() {
-    let message = "Stake Amount to CNTStaker ";
+    let message = "Stake Amount to CNTStaker. Allowance aaya : " + this.props.allowance;
     return `
         <div class="ui container">
           <div class="ui segment">
@@ -23,7 +23,7 @@ class Token {
 }
 
 web3.tokens.dataChanged = (oldTokens, updatedTokens, tokenCardId) => {
-  const currentTokenInstance = web3.tokens.data.currentInstance;
+  const currentTokenInstance = updatedTokens.currentInstance;
   document.getElementById(tokenCardId).innerHTML = new Token(currentTokenInstance).render();
 };
 
